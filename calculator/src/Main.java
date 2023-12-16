c46532a43840f0deceef2799303647a947e63214
Taiwo <taiwosodiqrazaq@gmail.com>
2023-12-16 13:47:11 +0000

    added a calc function

---
+++ calculator/src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        calc(8,6);
+    }
+    public static void calc(int firstNumber, int secondNumber){
+
+        int Answer = firstNumber + secondNumber;
+
+        System.out.print("Answer: " +Answer);
+    }
+}
