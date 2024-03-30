18519dee0a76025996a786bb94c03ea75e89aa66
Taiwo <taiwosodiqrazaq@gmail.com>
2024-03-30 13:25:28 +0000

    comment out calc and change sub class to choose

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -1,19 +1,15 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main extends sub {
+public class Main extends choose {
     public static void main(String[] args) {
 
-        calculator(10,16);
+      //  calculator(10,16);
         subtract(2,3);
     }
-    public static void calculator(int firstNumber, int secondNumber){
-
-        int Answer = firstNumber + secondNumber;
-
-        System.out.print("Answer: " +Answer);
-        //yes bro
-    }
-
-    //today is the day
-    String  name = "taye";
+//    public static void calculator(int firstNumber, int secondNumber){
+//
+//        int Answer = firstNumber + secondNumber;
+//
+//        System.out.print("Answer: " +Answer);
+//    }
 }
--- calculator/src/choose.java
+++ calculator/src/choose.java
@@ -1,11 +1,9 @@
-public class sub {
+public class choose {
 
     public static void subtract(int num1, int num2){
 
-        int ans = (num1 + num2);
+        int ans = (num1 - num2);
 
         System.out.print(ans);
     }
-
-
 }
