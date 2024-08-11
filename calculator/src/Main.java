a84fc47a4db5ad9d0b3adf85f8bc1dccdb051d0a
Taiwo <taiwosodiqrazaq@gmail.com>
2024-08-11 12:19:26 +0100

    change subtraction method to addition

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -1,16 +1,16 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main extends sub {
+public class Main extends add {
     public static void main(String[] args) {
 
         calculator(10,16);
-        subtract(2,3);
+        addition(2,3);
     }
     public static void calculator(int firstNumber, int secondNumber){
 
         int Answer = firstNumber + secondNumber;
 
-        System.out.print("Answer: " +Answer);
+        System.out.println("Answer: " +Answer);
         //yes bro
     }
 }
---
+++ calculator/src/add.java
@@ -0,0 +1,11 @@
+public class add {
+
+    public static void addition(int num1, int num2){
+
+        int ans = (num1 + num2);
+
+        System.out.println(ans);
+    }
+
+
+}
