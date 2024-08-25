f296bb2aa4b738aad8dda29614d97b0302177fe0
Amin <salis.aminu@yahoo.com>
2024-08-25 11:33:10 +0100

    add subtraction

--- calculator/src/add.java
+++ calculator/src/add.java
@@ -7,5 +7,10 @@ public class add {
         System.out.println(ans);
     }
 
+    public static void subtraction (int num1, int num2){
 
+        int ans = (num1 - num2);
+
+        System.out.println(ans);
+    }
 }
