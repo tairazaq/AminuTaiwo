4652c74e146e58128c32870045c384c3d7f7c2af
Taiwo <taiwosodiqrazaq@gmail.com>
2024-09-07 12:55:42 +0100

    add new project oyster card

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -4,10 +4,16 @@ public class Main extends choose {
     public static void main(String[] args) {
 
       //  calculator(10,16);
-        subtract(2,3);
-        addition(3,3);
-        multiply(3,3);
-        divide(25,5);
+
+//        subtract(2,3);
+//        addition(3,3);
+//        multiply(3,3);
+//        divide(25,5);
+
+        int i;
+        for(i=0; i<=10; i++){
+            System.out.println(i);
+        }
     }
 //    public static void calculator(int firstNumber, int secondNumber){
 //
@@ -15,4 +21,7 @@ public class Main extends choose {
 //
 //        System.out.print("Answer: " +Answer);
 //    }
+
+
+
 }
